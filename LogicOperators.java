aa5e5b35b42a52e5b7849e97bb0be0adef8c039a
Evgeniy <eurbagayev@mail.com>
2024-07-15 21:29:16 +0300

    throw exception

---
+++ LogicOperators.java
@@ -0,0 +1,11 @@
+public class LogicOperators {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 6;
+        boolean bool = true;
+
+        if (a == b || bool){
+            System.out.println("Это истина!");
+        }
+    }
+}
